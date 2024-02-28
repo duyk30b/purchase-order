5da34abb47e27c2a29a79ec643c8bf4f53e7d102
DUY Ngo Van (VTI.SnP) <duy.ngovan@vti.com.vn>
2024-02-28 14:48:56 +0700

    update soap-purchase-request.config.ts

--- src/components/sync-data/soap-purchase-request/soap-purchase-request.config.ts
+++ src/components/sync-data/soap-purchase-request/soap-purchase-request.config.ts
@@ -3,8 +3,8 @@ import { registerAs } from '@nestjs/config'
 export const SoapPurchaseRequestConfig = registerAs(
   'soap_purchase_request',
   () => ({
-    username: process.env.SAP_USERNAME || '_TEST_USER',
-    password: process.env.SAP_PASSWORD || 'Welcome1',
+    username: process.env.SAP_USERNAME || 'AE-0000002',
+    password: process.env.SAP_PASSWORD || 'Snp12345678',
     syncPurchaseRequestUri:
       process.env.SAP_PURCHASE_REQUEST_URI ||
       'static/wsdl/ManagePurchaseRequestIn_350617.wsdl',
